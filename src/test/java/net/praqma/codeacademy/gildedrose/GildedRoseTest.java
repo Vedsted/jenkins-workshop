335238f3c080bff53dd30d3278926079042010e6
Jonas Vedsted Sørensen <jonso16@student.sdu.dk>
2018-08-23 08:55:14 +0200

    test removed

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -52,11 +52,4 @@ public class GildedRoseTest {
         assertEquals("unaged brie", app.items[0].name);
     }
 
-    @Test
-    public void unagedBries() {
-        Item[] items = new Item[] { new Item("unaged brie", 5, 9) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("unaged brie", app.items[0].name);
-    }
 }
